8834496cb9ea9902827edf82f4ce1989822595fb
Jackson Welsh <me@jacksonwelsh.net>
2021-02-22 20:59:12 -0600

    i literally hate javascript why am i doing this to myself

--- scripts/gh-pages-deploy.js
+++ scripts/gh-pages-deploy.js
@@ -9,7 +9,7 @@ const fs = require("fs");
     await execa("npm", ["run", "build"]);
     // Understand if it's dist or build folder
     const folderName = fs.existsSync("dist") ? "dist" : "build";
-    fs.writeFile('CNAME', 'jacksonwel.sh', function(err) {});
+    fs.writeFile('CNAME', 'dist/jacksonwel.sh', function(err) {});
     await execa("git", ["--work-tree", folderName, "add", "--all"]);
     await execa("git", ["--work-tree", folderName, "commit", "-m", "gh-pages"]);
     console.log("Pushing to gh-pages...");
