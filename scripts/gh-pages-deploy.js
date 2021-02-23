eb3206f62faca917d0f24c62ac2dd37d0ddd67b7
Jackson Welsh <me@jacksonwelsh.net>
2021-02-22 20:50:18 -0600

    i heard you like cnames

--- scripts/gh-pages-deploy.js
+++ scripts/gh-pages-deploy.js
@@ -3,12 +3,13 @@ const execa = require("execa");
 const fs = require("fs");
 (async () => {
   try {
-    await execa("git", ["checkout", "gh-pages"]);
+    await execa("git", ["checkout", "--orphan", "gh-pages"]);
     // eslint-disable-next-line no-console
     console.log("Building started...");
     await execa("npm", ["run", "build"]);
     // Understand if it's dist or build folder
     const folderName = fs.existsSync("dist") ? "dist" : "build";
+    await execa("echo", ["'jacksonwel.sh'", ">", "CNAME"]);
     await execa("git", ["--work-tree", folderName, "add", "--all"]);
     await execa("git", ["--work-tree", folderName, "commit", "-m", "gh-pages"]);
     console.log("Pushing to gh-pages...");
